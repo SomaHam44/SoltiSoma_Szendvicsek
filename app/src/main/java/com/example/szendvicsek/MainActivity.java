ea98e83715964a1676c881ba934ad2a3d58e872a
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-12-01 19:59:10 +0100

    MainActivity működése

--- app/src/main/java/com/example/szendvicsek/MainActivity.java
+++ app/src/main/java/com/example/szendvicsek/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.szendvicsek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnFelvetel;
@@ -17,6 +20,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+
+        btnFelvetel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent felvetelre = new Intent(MainActivity.this, InsertActivity.class);
+                startActivity(felvetelre);
+                finish();
+            }
+        });
+
+        btnKeres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String arString = editAr.getText().toString().trim();
+                if (arString.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "A mező kitöltése kötelező!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    try {
+                        if (!arString.isEmpty()) {
+                            Intent keresesre = new Intent(MainActivity.this, SearchResultActivity.class);
+                            startActivity(keresesre);
+                            finish();
+                        }
+
+                    }
+                    catch (NumberFormatException e) {
+                        Toast.makeText(getApplicationContext(), "Az árnak számnak kell lennie!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 
     private void init() {
