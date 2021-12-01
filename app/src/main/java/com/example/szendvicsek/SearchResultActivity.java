57e10dabb92944f6226842783d1f2a8ab02350d9
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-12-01 20:33:26 +0100

    SearchResultActivity adattagok, init függvény, visszatero gomb OnClickListenere

--- app/src/main/java/com/example/szendvicsek/SearchResultActivity.java
+++ app/src/main/java/com/example/szendvicsek/SearchResultActivity.java
@@ -2,13 +2,36 @@ package com.example.szendvicsek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SearchResultActivity extends AppCompatActivity {
+    private TextView textKereso;
+    private Button btnVisszateres;
+    private DBHelper adatbazis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
+        init();
+
+        btnVisszateres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent visszamenni = new Intent(SearchResultActivity.this, MainActivity.class);
+                startActivity(visszamenni);
+                finish();
+            }
+        });
+    }
+
+    private void init() {
+        textKereso = findViewById(R.id.text_szendvicsek);
+        btnVisszateres = findViewById(R.id.btn_visszatero);
+        adatbazis = new DBHelper(this);
     }
 }
