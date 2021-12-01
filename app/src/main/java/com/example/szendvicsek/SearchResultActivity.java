2023a4423edbe8805091b3db76d2e479b387065d
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-12-01 20:40:01 +0100

    Kiegészítés

--- app/src/main/java/com/example/szendvicsek/SearchResultActivity.java
+++ app/src/main/java/com/example/szendvicsek/SearchResultActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,6 +19,7 @@ public class SearchResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
         init();
+        textKereso.setMovementMethod(new ScrollingMovementMethod());
 
         btnVisszateres.setOnClickListener(new View.OnClickListener() {
             @Override
