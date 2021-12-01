95dc1f797171b80a1cd257f3e4ff0d860c244013
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-12-01 20:08:50 +0100

    Módosítás

--- app/src/main/java/com/example/szendvicsek/DBHelper.java
+++ app/src/main/java/com/example/szendvicsek/DBHelper.java
@@ -24,7 +24,7 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String sql = " CREATE TABLE " + TABLE_NAME + " ( " + COL_ID
-                + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
+                + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     COL_NEV + " TEXT NOT NULL, " + COL_LEIRAS +
                 " TEXT NOT NULL, " + COL_ELKESZITES + " INTEGER NOT NULL, " +
                 COL_AR + " INTEGER NOT NULL " + ")";
