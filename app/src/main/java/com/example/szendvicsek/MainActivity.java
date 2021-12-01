83cbe14de72ec80c4aa994f8201ab0fdd8e9c35e
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-12-01 19:50:12 +0100

    MainActivity init metódus, privát adattagok

--- app/src/main/java/com/example/szendvicsek/MainActivity.java
+++ app/src/main/java/com/example/szendvicsek/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.szendvicsek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnFelvetel;
+    private Button btnKeres;
+    private EditText editAr;
+    private DBHelper adatbazis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        btnFelvetel = findViewById(R.id.btn_felvetel);
+        btnKeres = findViewById(R.id.btn_kereses);
+        editAr = findViewById(R.id.edit_ar);
+        adatbazis = new DBHelper(this);
     }
 }
